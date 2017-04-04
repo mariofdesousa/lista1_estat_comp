93ba3746272cf9b2d120493e5e2826ee5869758f
mariofdesousa <mariofdesousa@gmail.com>
2017-04-03 21:45:03 -0300

    Questão 8 feita
    Conferir a questão 8. Fiquei em dúvida na parte da antitética.

--- Estatistica computacional.R
+++ Estatistica computacional.R
@@ -1,8 +1,5 @@
 rm(list=ls())
-
-
 ## Questão 7
-
 #############################################
 n      <- 1e6
 rn     <- runif(n,0,pi/3)
@@ -17,6 +14,29 @@ vv <- 1/2  ## valor verdadeiro
 erro = abs(estMC - vv)
 print(erro)
 ## 2.891086e-06
+
+##############################################
+
+rm(list=ls())
+## Questão 8
+#############################################
+n          <- 500000
+rn         <- runif(n)
+integral   <- ((exp(-rn))/(1+(rn^2)))
+MCconv     <-  mean(integral) ##metodo convencional
+varconv    <- (1/n) * sum((MCconv-integral)^2)
+
+rn1        <- 1-rn
+int1       <- integral
+int2       <- ((exp(-rn1))/(1+(rn1^2)))
+MCant      <- mean((int1 + int2)/2)  
+varant     <- var(int1 + int2) ## Metodo das variaveis antiteticas
+
+## Comparando os valores
+
+vargain = ((varconv - varant)/varconv)
+print(vargain)
+## 0.927129 reducao de 92.71% da variancia
 
 ##############################################
 
